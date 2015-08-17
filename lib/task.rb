792e8c10bf70a1f7cdfa1954c94245f445c9774f
Ryan Broughan & Jason Lane <student@epicodus.com>
2015-08-17 11:27:02 -0700

    Add list_id to class Task

--- lib/task.rb
+++ lib/task.rb
@@ -1,9 +1,10 @@
 class Task
 
-  attr_reader :description
+  attr_reader :description, :list_id
 
   def initialize(attributes={})
     @description = attributes.fetch(:description, "")
+    @list_id = attributes.fetch(:list_id).to_i
   end
 
   def ==(another_task)
@@ -15,13 +16,15 @@ class Task
     tasks =[]
     returned_tasks.each do |task|
       description = task.fetch('description')
-      tasks.push(Task.new({ description: description }))
+      list_id = task.fetch('list_id')
+
+      tasks.push(Task.new({ description: description, list_id: list_id }))
     end
     tasks
   end
 
   def save
-    DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}');")
+    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
   end
 
   def ==(another_task)
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -13,8 +13,8 @@ end
 describe Task do
   describe '#==' do
     it 'is the same task if it has the same description' do
-      task1 = Task.new({description: 'learn SQL'})
-      task2 = Task.new({description: 'learn SQL'})
+      task1 = Task.new({description: 'learn SQL', list_id: 1 })
+      task2 = Task.new({description: 'learn SQL', list_id: 1 })
       expect(task1).to eq task2
     end
   end
@@ -28,9 +28,16 @@ describe Task do
 
   describe '#save' do
     it 'adds a task to the array of saved tasks' do
-      test_task = Task.new({ description: 'description' })
+      test_task = Task.new({ description: 'description', list_id: 1 })
       test_task.save
       expect(Task.all).to eq [test_task]
+    end
+  end
+
+  describe '#list_id' do
+    it 'lets you read the list ID out' do
+      test_task = Task.new({ description: 'learn SQL', list_id: 1 })
+      expect(test_task.list_id).to eq 1
     end
   end
 
