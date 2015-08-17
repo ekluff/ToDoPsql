42f5789a79706e1c6253c92c19eb634f2ff94971
Ryan Broughan & Jason Lane <student@epicodus.com>
2015-08-17 11:30:23 -0700

    DRY up specs, by factoring repeat code to spec_helper.rb

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,14 +1,6 @@
-require 'rspec'
-require 'pg'
-require 'list'
+require 'spec_helper'
 
-DB = PG.connect({dbname: 'to_do_test'})
 
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM lists *;")
-  end
-end
 
 describe List do
   describe '.all' do
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,14 +1,6 @@
-require 'rspec'
-require 'pg'
-require 'task'
+require 'spec_helper'
 
-DB = PG.connect({dbname: 'to_do_test'})
 
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec('DELETE FROM tasks *;')
-  end
-end
 
 describe Task do
   describe '#==' do
