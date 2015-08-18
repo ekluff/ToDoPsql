8a9f4a121f5db4a2c23a0e8255324a4779be58dd
Ryan Broughan & Evan Clough <student@epicodus.com>
2015-08-18 10:48:52 -0700

    Add index view and basic routes

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
-require 'sinatra' 
+require 'sinatra'
 
 get '/'  do
+	@lists = List.all
+	erb(:index)
+end
+
+get('list/new') do
+	erb(:list_form)
+end
+
+post('list/new') do
+	category = params.fetch('category')
+	name = params.fetch('name')
+
+	List.new({category: category, name: name}).save
+
+	@lists = List.all
+
 	erb(:index)
 end
---
+++ spec/integration_spec.rb
@@ -0,0 +1,15 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('adding a new list', {:type => :feature}) do
+  it('allows a user to click a list to see the tasks and details for it') do
+    visit('/')
+    click_link('Add New List')
+    fill_in('name', with: 'Epicodus Work')
+    fill_in('category', with: 'School')
+    click_button('Add List')
+    expect(page).to have_content('Epicodus Work')
+  end
+end
